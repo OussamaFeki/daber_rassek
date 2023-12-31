f2772bfae6dc254efe2301aeb692fa372638a83b
Oussama Fekih Hassen <hssanoussama@gmail.com>
2023-12-31 22:00:02 +0100

    correct the mean of rating

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -4,11 +4,11 @@ const Review = require('../models/reviews');
 // Create a new review
 exports.createReview = async (req, res) => {
   try {
-    const { clientId, employeeId, rating, comment } = req.body;
+    const { clientId, employeeId, rating} = req.body;
     if (rating < 0 || rating > 5) {
         return res.status(400).json({ error: 'Rating must be between 0 and 5' });
       }
-    const review = new Review({ clientId, employeeId, rating, comment });
+    const review = new Review({ clientId, employeeId, rating});
     await review.save();
     res.status(201).json({ message: 'Review added successfully' });
   } catch (error) {
@@ -21,7 +21,7 @@ exports.createReview = async (req, res) => {
 exports.getAverageRatingForEmployee = async (req, res) => {
   try {
     const { employeeId } = req.params;
-    const averageRating = await calculateAverageRating(employeeId);
+    const averageRating = await calculateMeanRating(employeeId);
     res.status(200).json({ averageRating });
   } catch (error) {
     console.error(error);
@@ -30,23 +30,22 @@ exports.getAverageRatingForEmployee = async (req, res) => {
 };
 
 // Helper function to calculate average rating for a specific employee
-async function calculateAverageRating(employeeId) {
+// Helper function to calculate average rating for a specific employee
+// Helper function to calculate average rating for a specific employee
+async function calculateMeanRating(employeeId) {
   try {
-    const result = await Review.aggregate([
-      {
-        $match: { employeeId: mongoose.Types.ObjectId(employeeId) },
-      },
-      {
-        $group: {
-          _id: null,
-          averageRating: { $avg: '$rating' },
-        },
-      },
-    ]);
+    const reviews = await Review.find({employeeId:employeeId});
 
-    return result.length > 0 ? result[0].averageRating : 0;
+    if (reviews.length === 0) {
+      return 0; // Return 0 if there are no reviews for the employee
+    }
+
+    const totalRating = reviews.reduce((sum, review) => sum + review.rating, 0);
+    const meanRating = totalRating / reviews.length;
+
+    return meanRating;
   } catch (error) {
     console.error(error);
-    throw new Error('Error calculating average rating');
+    throw new Error('Error calculating mean rating');
   }
 }
--- backend/routers/reviewRoutes.js
+++ backend/routers/reviewRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router(); 
 const reviewController = require('../controllers/reviewController');
-router.post('/reviews',reviewController.createReview);
-router.get('/reviews/average-rating/:employeeId', reviewController.getAverageRatingForEmployee);
+router.post('/add',reviewController.createReview);
+router.get('/average-rating/:employeeId', reviewController.getAverageRatingForEmployee);
 module.exports = router;
